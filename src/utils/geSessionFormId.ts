9cb96c05f18213f75028595ce30d6cac0eeb036d
Jung Rupak <jungrupak@Jungs-MacBook-Pro.local>
2025-11-25 18:21:52 +0545

    typeform id set to default

--- src/utils/geSessionFormId.ts
+++ src/utils/geSessionFormId.ts
@@ -1,15 +1,18 @@
 export function getSessionFormId() {
-  const FORM_A = "GYkOukSo"; // session 1 form
-  const FORM_B = "rUGJngpI"; // session 2 form
+
+  const formIframeID = "GYkOukSo"
+  return formIframeID;
+  // const FORM_A = "GYkOukSo"; // session 1 form
+  // const FORM_B = "rUGJngpI"; // session 2 form
 
   // Check if formId already exists in sessionStorage
-  let formId = sessionStorage.getItem("formId");
+  //let formId = sessionStorage.getItem("formId");
 
   // If not assigned yet, randomly pick one (50|50 split)
-  if (!formId) {
-    formId = Math.random() < 0.5 ? FORM_A : FORM_B;
-    sessionStorage.setItem("formId", formId);
-  }
+  // if (!formId) {
+  //   formId = Math.random() < 0.5 ? FORM_A : FORM_B;
+  //   sessionStorage.setItem("formId", formId);
+  // }
 
-  return formId;
+  // return formId;
 }
