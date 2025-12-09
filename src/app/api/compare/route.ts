3435e643b6a5a40af625f5db03e5cc03adf9d986
Jung Rupak <jungrupak@Jungs-MacBook-Pro.local>
2025-12-09 12:26:10 +0545

    route endpoing fix

--- src/app/api/compare/route.ts
+++ src/app/api/compare/route.ts
@@ -1,8 +1,13 @@
 import { NextResponse } from "next/server";
+import https from "https";
 
 export async function GET() {
 
   try {
+
+    const agent = new https.Agent({
+      rejectUnauthorized: false, // <-- allows self-signed certificate
+    });
     
      const res = await fetch("https://scoreboard.argamon.com:8443/api/costs/comparison?period=7d&symbols=All%20pairs&mode=day&commission=true", {
       cache: "no-store",
