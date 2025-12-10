3f18c08580061af39f7c6e27a337c430b73a0af4
Jung Rupak <jungrupak@Jungs-MacBook-Pro.local>
2025-12-10 21:14:31 +0545

    fix main navigation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,15 +3,14 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "./style.module.scss";
 import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
 import { MegaMenuItems } from "@/utils/menu-item";
 import TypeformButton from "../ui/typeForm";
+import Navigation from "../nav/Nav";
+import MobileNav from "../mobileNav/MobileNav";
 
 export default function Header() {
   const [isSticky, setIsSticky] = useState(false);
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
-  const [isMegaOpen, setIsMegaOpen] = useState(false);
-  const [megaMenu, setMegaMenu] = useState(0);
   const [mobileMenu, setMobileMenu] = useState(false);
 
   // ✅ MegaMenuItems should be plain array
@@ -35,7 +34,7 @@ export default function Header() {
         }`}
       >
         <div
-          className={`${styles.ap_container} ap_container flex items-center py-8 max-lg:px-5 max-xl:px-5 max-lg:py-5 max-xl:gap-10 justify-between`}
+          className={`${styles.ap_container} ap_container flex items-center py-5 max-lg:px-5 max-xl:px-5 max-lg:py-5 max-xl:gap-10 justify-between`}
         >
           {/* Logo */}
           <div className={`${styles.ap_logo}`}>
@@ -50,121 +49,30 @@ export default function Header() {
           </div>
 
           {/* Nav */}
-          <div
-            className={`${styles.middleItem} ${
-              mobileMenu ? styles.ActiveNav : ""
-            }`}
-          >
-            <div className={`${styles.apNavWrapper}`}>
-              {/* Mobile Nav Header */}
 
-              <div className={`${styles.resNavHeader}`}>
-                <div className={`${styles.ap_logo}`}>
-                  <Link href="/">
-                    <Image
-                      src="/img/logo-text.svg"
-                      alt="Afterprime Logo"
-                      width={135}
-                      height={25}
-                    />
-                  </Link>
-                </div>
-                <div className="flex gap-5 ml-auto">
-                  <span
-                    className={`${styles.closeIcon} translate-x-[15px]`}
-                    onClick={() => setMobileMenu(false)}
-                  >
-                    +
-                  </span>
-                </div>
-              </div>
-
-              {/* Main Nav */}
-              <nav className={`${styles.ap_nav}`}>
-                <ul className="flex gap-12">
-                  {MegaItems.map((navItem, index) => (
-                    <li
-                      key={index}
-                      className={`max-md:w-full relative ${
-                        activeIndex === index ? styles.active : ""
-                      }`}
-                      // Desktop hover
-                      onMouseEnter={() => {
-                        if (window.innerWidth >= 1024) {
-                          setIsMegaOpen(true);
-                          setActiveIndex(index);
-                          setMegaMenu(index);
-                        }
-                      }}
-                    >
-                      {/* Click for Mobile */}
-                      <span
-                        className="cursor-pointer flex items-center gap-4 max-lg:px-[10px]"
-                        onClick={() => {
-                          if (window.innerWidth < 1024) {
-                            if (activeIndex === index) {
-                              setIsMegaOpen(false);
-                              setActiveIndex(null);
-                            } else {
-                              setIsMegaOpen(true);
-                              setActiveIndex(index);
-                              setMegaMenu(index);
-                            }
-                          }
-                        }}
-                      >
-                        {navItem.menu}
-
-                        {navItem.category && (
-                          <svg
-                            width="9"
-                            height="10"
-                            viewBox="0 0 9 10"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <mask id="path-1-inside-1_1406_223" fill="white">
-                              <path d="M4.39258 0.607727L8.78485 5L4.39258 9.39227L0.000305411 5L4.39258 0.607727Z" />
-                            </mask>
-                            <path
-                              d="M4.39258 9.39227L3.33192 10.4529L4.39258 11.5136L5.45324 10.4529L4.39258 9.39227ZM8.78485 5L7.72419 3.93934L3.33192 8.33161L4.39258 9.39227L5.45324 10.4529L9.84551 6.06066L8.78485 5ZM4.39258 9.39227L5.45324 8.33161L1.06097 3.93934L0.000305411 5L-1.06035 6.06066L3.33192 10.4529L4.39258 9.39227Z"
-                              fill="#FDFCF7"
-                              mask="url(#path-1-inside-1_1406_223)"
-                            />
-                          </svg>
-                        )}
-                      </span>
-                    </li>
-                  ))}
-                </ul>
-              </nav>
-
-              {/* Mobile Footer */}
-              <div className={`${styles.resNavFooter}`}>
-                <a
-                  className={`ap_button washed small`}
-                  href="https://app.afterprime.com/login"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Member Login
-                </a>
-                <TypeformButton buttonText="Request Invite" size="small" />
-              </div>
-            </div>
+          <div className="max-[1204px]:hidden">
+            <Navigation menus={MegaMenuItems} />
           </div>
+
+          <MobileNav
+            menus={MegaMenuItems}
+            customClass={`${
+              mobileMenu ? styles.activeMenu : styles.notActiveMenu
+            }`}
+            onClick={() => setMobileMenu(false)}
+          />
 
           {/* Right Side (Desktop) */}
           <div className={`${styles.ap_header_right} max-[1204px]:hidden`}>
             <div className="flex items-center gap-4">
-              <a
+              <Link
                 className={`ap_button washed small`}
                 href="https://app.afterprime.com/login"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Member Login
-              </a>
+              </Link>
               <TypeformButton buttonText="Request Invite" size="small" />
             </div>
           </div>
@@ -191,88 +99,6 @@ export default function Header() {
             </div>
           </div>
         </div>
-
-        {/* Mega Menu Area */}
-        {isMegaOpen && activeIndex !== null && (
-          <div
-            className={`${styles.megaMenu}`}
-            onMouseEnter={() => {
-              if (window.innerWidth <= 1204) {
-                setIsMegaOpen(true);
-              }
-            }}
-            onMouseLeave={() => {
-              if (window.innerWidth >= 1204) {
-                setIsMegaOpen(false);
-                setActiveIndex(null);
-                setMegaMenu(0);
-              }
-            }}
-          >
-            <div className={`${styles.megaMenuItemsContainer} ap_container`}>
-              <div className={`${styles.subNavHeader} flex`}>
-                <span
-                  className={`${styles.backArrow}`}
-                  onClick={() => {
-                    setIsMegaOpen(false);
-                    setActiveIndex(null);
-                  }}
-                >
-                  <svg
-                    width="9"
-                    height="10"
-                    viewBox="0 0 9 10"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <mask id="path-1-inside-1_1406_223" fill="white">
-                      <path d="M8.78467 5L4.3924 9.39227L0.000122171 5L4.39239 0.607727L8.78467 5Z" />
-                    </mask>
-                    <path
-                      d="M0.000122171 5L-1.06054 3.93934L-2.1212 5L-1.06054 6.06066L0.000122171 5ZM4.3924 9.39227L5.45306 8.33161L1.06078 3.93934L0.000122171 5L-1.06054 6.06066L3.33174 10.4529L4.3924 9.39227ZM0.000122171 5L1.06078 6.06066L5.45306 1.66839L4.39239 0.607727L3.33173 -0.452933L-1.06054 3.93934L0.000122171 5Z"
-                      fill="#FDFCF7"
-                      mask="url(#path-1-inside-1_1406_223)"
-                    />
-                  </svg>
-                  back
-                </span>
-              </div>
-
-              <div
-                className={`${styles.menuChild} flex max-lg:flex-col max-lg:gap-5 max-lg:max-h-[88vh] max-lg:overflow-y-auto`}
-              >
-                {/* Mega Info */}
-
-                {/* Mega Categories */}
-                {MegaItems[megaMenu]?.category?.map((cat, index) => (
-                  <div key={index} className={`${styles.menuCat_item}`}>
-                    <h2>{cat.categoryName}</h2>
-                    <ul>
-                      {cat.catMenuItems.map((li, indx) => (
-                        <li key={indx}>
-                          <Link
-                            href={li.pageUrl}
-                            onClick={() => {
-                              setMobileMenu(false);
-                              setIsMegaOpen(false);
-                              setActiveIndex(null);
-                            }}
-                            {...(li.menuItem === "Help Center"
-                              ? { target: "_blank", rel: "noopener noreferrer" }
-                              : {})}
-                          >
-                            {li.menuItem}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        )}
-        {/* Mega Menu Area ends */}
       </header>
     </>
   );
---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,104 @@
+"use client";
+import styles from "./Nav.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import DropdownIcon from "../ui/DropdownIcon";
+import { useEffect, useState } from "react";
+
+interface CategoryItem {
+  menuItem?: string;
+  pageUrl?: string;
+}
+interface Category {
+  categoryName?: string;
+  catMenuItems?: CategoryItem[];
+}
+
+interface Menu {
+  menu?: string;
+  menuInfos?: {
+    title: string;
+    description: string;
+    btnText: string;
+    btnLink: string;
+  };
+  category?: Category[];
+}
+
+interface MenuItems {
+  menus?: Menu[];
+}
+
+export default function Navigation({ menus }: MenuItems) {
+  const [megaMenu, setMegaMenu] = useState(false);
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
+  return (
+    <div>
+      <nav>
+        <ul className="flex gap-12">
+          {menus?.map((menu, idx) => (
+            <li
+              key={idx}
+              className={`${styles.itemLevelParent} max-md:w-full relative `}
+              onMouseEnter={() => {
+                setMegaMenu(true);
+                setOpenIndex(idx);
+              }}
+              onMouseLeave={() => {
+                setMegaMenu(false);
+                setOpenIndex(null);
+              }}
+            >
+              <span className="cursor-pointer flex items-center gap-4 max-lg:px-[10px]">
+                {menu.menu}
+                {menu?.category?.length ? <DropdownIcon /> : null}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      {/* Mega Menu */}
+      {megaMenu && openIndex !== null && (
+        <div
+          className={`${styles.megaMenuWrapper}`}
+          onMouseEnter={() => {
+            setMegaMenu(true);
+            setOpenIndex(openIndex);
+          }}
+          onMouseLeave={() => {
+            setMegaMenu(false);
+            setOpenIndex(null);
+          }}
+        >
+          <div className={`${styles.megaMenuItemsContainer}`}>
+            {/* This is allu arjun {openIndex} */}
+            <div className={`${styles.catWrapper} flex gap-0`}>
+              {menus?.[openIndex]?.category?.map((cat, i) => (
+                <div key={i} className={`${styles.menuCat_item}`}>
+                  <h2>{cat.categoryName}</h2>
+                  <ul>
+                    {cat.catMenuItems?.map((menuItem, index) => (
+                      <li
+                        key={index}
+                        onClick={() => {
+                          setMegaMenu(false);
+                          setOpenIndex(null);
+                        }}
+                      >
+                        <Link href={menuItem.pageUrl || "/"}>
+                          {menuItem.menuItem}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
