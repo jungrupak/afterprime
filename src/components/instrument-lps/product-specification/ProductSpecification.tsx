ce85b3f216642183e1c7b07727b0e196d4987a72
Jung Rupak <jungrupak@Jungs-MacBook-Pro.local>
2026-01-30 11:12:42 +0545

    specsheet updated dynamically

--- src/components/instrument-lps/product-specification/ProductSpecification.tsx
+++ src/components/instrument-lps/product-specification/ProductSpecification.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
-import { getBrokerCompareData } from "@/lib/getBrokersToCompare";
+import { AP_FX_PAIRS } from "@/data/ap-fx-pairs-specs";
 import styles from "./ProductSpecification.module.scss";
 
 interface Specification {
@@ -14,18 +13,14 @@ export default function ProductSpecification({ instrument }: Specification) {
   //##
   if (!instrument) return;
 
-  //####
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["cost-comparison", instrument],
-    queryFn: () => getBrokerCompareData(instrument!),
-    enabled: !!instrument, //prevents undefined crash
-    staleTime: CACHE_TTL, // fresh for 2 minutes
-    gcTime: 10 * 60 * 1000, // cache stays for 10 minutes
-  });
+  // ####
+  const specData = AP_FX_PAIRS;
+  const selectedInstrument = AP_FX_PAIRS.find(
+    (item) => item.Symbol === instrument,
+  );
   //####
 
-  const averageSpread = data?.brokers?.[0]?.cost ?? 0;
-  const assetType = data?.rebate?.product ?? undefined;
+  console.log("spec-data:", selectedInstrument);
 
   return (
     <section className={`md:py-20!`}>
@@ -40,68 +35,13 @@ export default function ProductSpecification({ instrument }: Specification) {
         <div className={`${styles.costBreakDownTable}`}>
           <table cellPadding={"0"} cellSpacing={"0"} border={0}>
             <tbody>
-              <tr>
-                <td>Asset Class</td>
-                <td>{assetType}</td>
-              </tr>
-              <tr>
-                <td>Product Type</td>
-                <td>XX</td>
-              </tr>
-              <tr>
-                <td>Commission</td>
-                <td>Zero</td>
-              </tr>
-              <tr>
-                <td>
-                  Flow Rewards<sup>TM</sup>
-                </td>
-                <td>0.50 USD</td>
-              </tr>
-              <tr>
-                <td>Execution Speed</td>
-                <td> &less; 50ms </td>
-              </tr>
-              <tr>
-                <td>Contract Size</td>
-                <td>100,000</td>
-              </tr>
-              <tr>
-                <td>Min. Lot</td>
-                <td>XX</td>
-              </tr>
-              <tr>
-                <td>Max. Lots</td>
-                <td>XX</td>
-              </tr>
-              <tr>
-                <td>Lot Step</td>
-                <td>XX</td>
-              </tr>
-              <tr>
-                <td>Max Leverage</td>
-                <td>1:400</td>
-              </tr>
-              <tr>
-                <td>Swap Type & 3-Day Swap</td>
-                <td>Points based, Wednesday</td>
-              </tr>
-              <tr>
-                <td>Trading Hours</td>
-                <td>Monday to Friday 00:00 to 23:59 GMT plus 2</td>
-              </tr>
-              <tr>
-                <td>Platforms</td>
-                <td>MT4, MT5, WebTrader, TraderEvolution, FIX Api</td>
-              </tr>
-              <tr>
-                <td>Licensing</td>
-                <td>
-                  Afterprime Ltd (Seychelles company registration number
-                  8426189-1) is a Securities Dealer, authorised by the Financial
-                  Service Authority (FSA) with license #SD057
-                </td>
-              </tr>
+              {selectedInstrument &&
+                Object.entries(selectedInstrument).map(([key, value]) => (
+                  <tr key={key}>
+                    <td>{key}</td>
+                    <td>{value}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
