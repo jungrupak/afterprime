fc0abebb2f4b12d92d62590c2441abbf13b384b6
Jung Rupak <jungrupak@Jungs-MacBook-Pro.local>
2026-02-10 09:48:23 +0545

    global cta

--- src/components/acfFieldGroups/bottom-cta/BottomCta.tsx
+++ src/components/acfFieldGroups/bottom-cta/BottomCta.tsx
@@ -1,66 +1,34 @@
-import dynamic from "next/dynamic";
-import styles from "./style.module.scss";
-import Btn from "@/components/ui/Button";
-import type { PageFieldGroups } from "@/types/blocks";
-import TypeformButton from "@/components/ui/typeForm";
-
-type CtaProps = PageFieldGroups["bottom_cta"];
-
-export function BottomCta(props: CtaProps) {
-  const { section_title, section_paragraph, card_apply, card_referal } =
-    props || {};
-
+import Cta from "@/components/instrument-lps/cta/Cta";
+import TypeformButton from "@/components/instrument-lps/typeform-btn/typeForm";
+import styles from "@/components/instrument-lps/cta/Cta.module.scss";
+export function BottomCta() {
   return (
-    <>
-      <section className={`${styles.section_cta} compact-section`}>
-        {/* grain bg effect */}
-        <div className="grainy_bg"></div>
-        {/* grain bg effect */}
-        <div className="ap_container_small">
-          <div className="max-w-[700px] mx-auto text-center">
-            <h2 className="h2-size mb-6">
-              Built on alignment,
-              <br />
-              not extraction.
+    <section className={`compact-section`}>
+      {/* grain bg effect */}
+      <div className="grainy_bg"></div>
+      {/* grain bg effect */}
+
+      <div className={`ap_container_small relative z-1 w-full`}>
+        <div
+          className={`${styles.bottomCta} flex flex-col justify-center items-center text-center`}
+        >
+          <div>
+            <h2 className={`md:mb-8! leading-[1]`}>
+              No Fine Print. Better Trading Economics.
             </h2>
-            <p className="paragraph max-w-2xl mx-auto mb-20 opacity-90">
-              Lowest costs, transparent execution, shared rewards.Value you
-              won&apos;t find anywhere else.
+            <p className={`paragraph mb-8 md:mb-10 opacity-80`}>
+              Built on transparency. Lowest total trading costs.
+              <br />
+              Execution you can measure. Rewards shared with you.
             </p>
-          </div>
-          {/* Cards */}
-          <div className="ap_cards_wrapper grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6 text-center md:mt-18 md:max-w-[900px] md:mx-auto">
-            <div className={`${styles.ctaCard} group`}>
-              <h3 className="">Apply</h3>
-              <p>
-                We review each application by hand.Only serious traders are
-                approved.
-              </p>
-
-              <TypeformButton buttonText="Request Invite" size="Regular" />
-            </div>
-
-            <div className={`${styles.ctaCard} group`}>
-              <h3 className="">Referral</h3>
-              <p>
-                Know an existing member? Their invite code gets you straight
-                through the door.
-              </p>
-
-              <a
-                className="ap_button ghost regular"
-                href="https://discord.com/invite/NKBcxyWzdM"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {card_referal?.cat_label || "Ask on Discord"}
-              </a>
+            <TypeformButton buttonText="Apply for Invite code" size="Regular" />
+            <div className={`mt-5 opacity-65`}>
+              Invite only access for approved trading profiles.
             </div>
           </div>
-          {/* Cards Ends */}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
 //
