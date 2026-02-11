3f2a391e12bfa2dba35179b342e3de88529d6975
zolabud <iconbet@gmail.com>
2026-02-11 11:27:08 +1100

    Update Cta.tsx

--- src/components/instrument-lps/cta/Cta.tsx
+++ src/components/instrument-lps/cta/Cta.tsx
@@ -24,7 +24,8 @@ export default function Cta() {
             </p>
             <TypeformButton buttonText="Apply for Invite code" size="Regular" />
             <div className={`mt-5 opacity-65`}>
-              Invite only access for approved trading profiles.
+              Invite only access for approved trading profiles.<br />
+              Get <a href="https://discord.com/invite/NKBcxyWzdM" target="_blank">invite from Discord</a> friend.
             </div>
           </div>
         </div>
