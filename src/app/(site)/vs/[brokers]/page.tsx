2d0f892dfc27eb4c477ac2291a98daf2444ca005
Jung Rupak <jungrupak@Jungs-MacBook-Pro.local>
2026-02-23 22:32:16 +0545

    broker dynamic page

--- src/app/(site)/vs/[brokers]/page.tsx
+++ src/app/(site)/vs/[brokers]/page.tsx
@@ -1,11 +1,15 @@
 import { wpFetch } from "@/utils/wpFetch";
 import type { WPPage } from "@/types/blocks";
 import { Metadata } from "next";
+import styles from "./Page.module.scss";
 import { notFound } from "next/navigation";
 import { getPageDataBySlug } from "@/data/getPageDataBySlug";
 import PageRenderer from "@/components/PageRender";
 import CostComparisonWithSelected from "./compare-ap-with-selected/CostComparison";
 import CompareWithMajors from "./compare-with-majors/CostComparison";
+import Button from "@/components/ui/Button";
+import TypeformButton from "@/components/ui/typeForm";
+import FaqCalc from "@/components/faq-calculators/Faq";
 
 // ISR
 export const revalidate = 60;
@@ -31,43 +35,63 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-const compareBrokerData = async (brokers: string) => {
-  try {
-    const res = await fetch(
-      `https://feed.afterprime.com/api/majors/by-competitor?contains=${brokers}`,
-      {
-        next: { revalidate: 80 },
-      },
-    );
-    if (!res.ok) {
-      throw new Error("Faild to load broker individual data");
-    }
-    const data = await res.json();
-    return data;
-  } catch (err) {
-    console.error("Faild to load broker individual data:", err);
-  }
-};
-
 //
 // 🔹 Page Component
 //
 export default async function ChildPage({ params }: Props) {
   const { brokers } = await params;
   if (!brokers) return;
-  const [pageData, brokerData] = await Promise.all([
-    getPageDataBySlug(brokers),
-    compareBrokerData(brokers),
-  ]);
+  const pageData = await getPageDataBySlug(brokers);
   if (!pageData) {
     notFound();
   }
 
+  const ctaBlockFields = pageData?.acf?.cta_block;
+  const heroBanner = pageData?.acf?.hero_banner_home;
+  const faqData = pageData?.acf?.faq_section?.q_and_a;
   return (
     <>
-      {/* This is dynamic block renderer form page */}
-      <PageRenderer pageData={pageData} />
-      {/* This is dynamic block renderer from page ends */}
+      {/* Inner Banner */}
+      <section
+        className={`${styles.innerBannerSection} h-auto! innerpage-banner`}
+      >
+        {/* grain bg effect */}
+        <div className="grainy_bg"></div>
+        {/* grain bg effect */}
+        <div className="ap_container_small flex items-center h-full">
+          <div className={`apBannerContent`}>
+            <h1 className="h1-size mt-10 lg:mt-15 max-w-[800px]">
+              <span className="font-[600]">{heroBanner?.banner_heading}</span>
+            </h1>
+            <div
+              className="paragraph max-w-[600px] lg:mt-8 opacity-80"
+              style={{ fontWeight: "300" }}
+              dangerouslySetInnerHTML={{
+                __html: heroBanner?.banner_paragraph || "&nbsp;",
+              }}
+            />
+
+            {heroBanner &&
+              (heroBanner?.is_type_form_cta === true ? (
+                <div className={`mt-8 md:mt-15`}>
+                  <TypeformButton buttonText="Get Invite Code" size="Large" />
+                </div>
+              ) : (
+                <div className={`mt-8 md:mt-15`}>
+                  <Button
+                    href={heroBanner?.banner_btn_url}
+                    varient="primary"
+                    size="regular"
+                    isArrowVisible={true}
+                  >
+                    {heroBanner?.banner_btn_text}
+                  </Button>
+                </div>
+              ))}
+          </div>
+        </div>
+      </section>
+      {/* Inner Banner Ends */}
 
       {/* Trading Cost Breakdown */}
       <section className={`compact-section`}>
@@ -88,6 +112,59 @@ export default async function ChildPage({ params }: Props) {
         </div>
       </section>
       {/* Trading Cost Breakdown Ends */}
+
+      {/* Dynamic Content Area */}
+      <section className={`compact-section`}>
+        <div className="grainy_bg"></div>
+        <div className="ap_container_small">
+          <div className={`${styles.pageEditorContent}`}>
+            <h2 className={`mt-0!`}>Spreads</h2>
+            <p>Dynamic narrative addresses:</p>
+            <ul>
+              <li>Spread differential</li>
+              <li>Cost per lot impact</li>
+              <li>How it affects total cost calculation</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      {/* Dynamic Content Area Ends */}
+
+      {/* CTA */}
+      <section className={`compact-section`}>
+        <div className="grainy_bg"></div>
+        <div className="ap_container_small">
+          <div
+            className={`${styles.bottomCta} flex flex-col justify-center items-center text-center`}
+          >
+            <div>
+              <h2
+                className={`text-[clamp(30px_,5vw_,50px)]! md:mb-8! leading-[1]`}
+              >
+                {ctaBlockFields?.title}
+              </h2>
+              <p
+                className={`paragraph mb-8 md:mb-10 opacity-80`}
+                dangerouslySetInnerHTML={{
+                  __html: ctaBlockFields?.paragraph || "",
+                }}
+              />
+              <Button
+                varient="primary"
+                size="regular"
+                href={ctaBlockFields?.button_url}
+              >
+                {ctaBlockFields?.button_text}
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* CTA Ends */}
+
+      {/* FAQs */}
+      <FaqCalc faqSubject="FAQ." data={faqData} />
+      {/* FAQs Ends */}
     </>
   );
 }
