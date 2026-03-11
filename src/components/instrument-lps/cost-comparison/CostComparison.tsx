bf88a2914c52047d2842ecdfd853a34e2ad4c3da
zolabud <iconbet@gmail.com>
2026-03-11 14:27:19 +1100

    Update CostComparison.tsx

--- src/components/instrument-lps/cost-comparison/CostComparison.tsx
+++ src/components/instrument-lps/cost-comparison/CostComparison.tsx
@@ -273,11 +273,15 @@ export default function CostComparison({ instrument }: { instrument: string }) {
                   )}
 
                   <div data-label={`Broker`} className={`col-span-3 relative`}>
-                  {broker.broker !== "Afterprime" && (
+                  {broker.broker === "Afterprime" ? (
+                    <span className="text-[14px] max-md:text-[16px] block">
+                      <strong>Afterprime</strong>
+                    </span>
+                  ) : (
                     <Link
                       href={`/vs/${brokerSlugMap[broker.broker] ?? ""}`}
                       scroll={true}
-                      className={`underline hover:no-underline text-[14px] max-md:text-[16px] block`}
+                      className="underline hover:no-underline text-[14px] max-md:text-[16px] block"
                     >
                       {broker.broker}
                     </Link>
