66ecf63fdd4d0adfd31c7a9d1c68d7db4fb508ef
Jung Rupak <jungrupak@Jungs-MacBook-Pro.local>
2026-04-13 07:08:18 +0545

    fixes for html accept in paragraph graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,19 @@
 import "./globals.scss";
 import { Blinker } from "next/font/google";
+import type { Viewport } from "next";
 
 const blinker = Blinker({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "600", "700", "800"], // choose the weights you need
 });
 
-export function generateViewport() {
-  return {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    minimumScale: 1,
-    themeColor: "#0c0c0d",
-  };
-}
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  themeColor: "#0c0c0d",
+};
 
 export default function RootLayout({
   children,
