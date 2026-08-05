87467d1ede6483e2ebe7eff84086556c69f85db7
Jungrupak <jungrupak@gmail.com>
2026-08-05 14:35:25 +0545

    mobile menu fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -66,11 +66,13 @@ export default function Header({ content }: { content: HeaderContent }) {
     body.style.position = "fixed";
     body.style.top = `-${scrollY}px`;
     body.style.width = "100%";
+    body.style.overflow = "hidden";
 
     return () => {
       body.style.position = "";
       body.style.top = "";
       body.style.width = "";
+      body.style.overflow = "";
       window.scrollTo(0, scrollY);
     };
   }, [mobileMenu]);
