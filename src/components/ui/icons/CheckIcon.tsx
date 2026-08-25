7db9f809e4c00e2c7ec9ea7fbe91c04a06d4ce78
Jungrupak <jungrupak@gmail.com>
2026-08-25 14:01:18 +0545

    feat: add CheckIcon for boosted comparison section

---
+++ src/components/ui/icons/CheckIcon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function CheckIcon() {
+  return (
+    <svg
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21 11C21 5.47715 16.5228 1 11 1C5.47715 1 1 5.47715 1 11C1 16.5228 5.47715 21 11 21V22C4.92487 22 0 17.0751 0 11C0 4.92487 4.92487 0 11 0C17.0751 0 22 4.92487 22 11C22 17.0751 17.0751 22 11 22V21C16.5228 21 21 16.5228 21 11Z"
+        fill="#22C55E"
+      />
+      <path
+        d="M6.5 11.5L9.5 14.5L15.5 8"
+        stroke="#22C55E"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
