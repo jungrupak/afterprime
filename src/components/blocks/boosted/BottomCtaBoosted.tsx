976d645842dd8479bbbc8bba51618371b91a0adf
Jungrupak <jungrupak@gmail.com>
2026-08-25 14:04:20 +0545

    feat: add boosted closing CTA and regulatory placeholder note

---
+++ src/components/blocks/boosted/BottomCtaBoosted.tsx
@@ -0,0 +1,32 @@
+"use client";
+import styles from "./BottomCtaBoosted.module.scss";
+import { boostedContent } from "./boostedContent";
+import { useInView } from "./useInView";
+
+export default function BottomCtaBoosted() {
+  const { bottomCta, footerNote } = boostedContent;
+  const { ref, isVisible } = useInView<HTMLDivElement>();
+
+  return (
+    <section className="compact-section">
+      <div
+        ref={ref}
+        className={`ap_container_small ${styles.wrapper} ${
+          isVisible ? styles.visible : ""
+        }`}
+      >
+        <div className={styles.panel}>
+          <div className={styles.copy}>
+            <h2 className="h2-size">{bottomCta.heading}</h2>
+            <p className={styles.body}>{bottomCta.body}</p>
+          </div>
+          {/* TODO: wire to TradeCore signup flow */}
+          <a href="#" className={styles.cta}>
+            {bottomCta.cta}
+          </a>
+        </div>
+        <p className={styles.footer_note}>{footerNote}</p>
+      </div>
+    </section>
+  );
+}
